6a2bc9413094970bb55a3dd5dc1ab6dc378c55b4
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-08-22 01:55:35 +0300

    Service Entity Done

---
+++ src/main/java/org/os/carcareservice/entity/Service.java
@@ -0,0 +1,62 @@
+package org.os.carcareservice.entity;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "services")
+public class Service {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer serviceId;
+
+    @Column(name = "name", nullable = false, length = 100)
+    private String serviceName;
+
+    @Column(name = "status" , nullable = false , length = 50)
+    private String serviceStatus;
+
+    @Column(name = "price", precision = 10, scale = 2, nullable = false)
+    private BigDecimal servicePrice;
+
+    public Service() {}
+
+    public Service(String serviceName, String serviceStatus, BigDecimal servicePrice) {
+        this.serviceName = serviceName;
+        this.serviceStatus = serviceStatus;
+        this.servicePrice = servicePrice;
+    }
+
+    public Integer getServiceId() {
+        return serviceId;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public String getServiceStatus() {
+        return serviceStatus;
+    }
+
+    public BigDecimal getServicePrice() {
+        return servicePrice;
+    }
+
+    public void setServiceId(Integer serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public void setServicePrice(BigDecimal servicePrice) {
+        this.servicePrice = servicePrice;
+    }
+
+    public void setServiceStatus(String serviceStatus) {
+        this.serviceStatus = serviceStatus;
+    }
+}
