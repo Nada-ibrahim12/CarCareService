d0691b44e17b10c630b13079a60cfba39f01e197
Nada-ibrahim12 <123187920+Nada-ibrahim12@users.noreply.github.com>
2025-08-21 21:32:37 +0300

    initialize

---
+++ src/main/java/org/os/carcareservice/CarCareServiceApplication.java
@@ -0,0 +1,13 @@
+package org.os.carcareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarCareServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/os/carcareservice/CarCareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.os.carcareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarCareServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
