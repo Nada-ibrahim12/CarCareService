ab20c117b883d04dcba944c8970c2ab33ea6e107
Roqaia Hassan <roqaiahassanahmed@gmail.com>
2025-08-25 12:59:22 +0300

    merge edit models to cutomerCar branch

--- src/main/java/org/os/carcareservice/entity/User.java
+++ src/main/java/org/os/carcareservice/entity/User.java
@@ -1,9 +1,25 @@
 package org.os.carcareservice.entity;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import jakarta.validation.constraints.*;
-
 import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+
 
 @Entity
 @Table(name = "users")
