34f8cff8affae5def50e0f155ae6a2020e33a018
Ephraim Youssef <ephraim.youssf@gmail.com>
2025-08-27 07:50:12 +0300

    Add .env

--- src/main/java/org/os/carcareservice/CarCareServiceApplication.java
+++ src/main/java/org/os/carcareservice/CarCareServiceApplication.java
@@ -1,5 +1,6 @@
 package org.os.carcareservice;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CarCareServiceApplication {
 
     public static void main(String[] args) {
+
+        Dotenv dotenv = Dotenv.load();
+
+        System.setProperty("DB_URL" , dotenv.get("DB_URL"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("API_PORT", dotenv.get("API_PORT"));
+        System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
+        System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+
         SpringApplication.run(CarCareServiceApplication.class, args);
     }
 
--- src/main/java/org/os/carcareservice/config/JwtService.java
+++ src/main/java/org/os/carcareservice/config/JwtService.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,11 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    private static final String JWT_SECRET = "404E635266556A586E3272357538782F413F4428472B4B6250645367566B5970";
+    @Value("${jwt.secret}")
+    private String JWT_SECRET;
+
+    @Value("${jwt.expiration}")
+    private Long JWT_EXPIRATION;
 
     public String generateToken(UserDetails userDetails) {
         return generateToken(new HashMap<>(), userDetails);
@@ -31,7 +36,7 @@ public class JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))    // ? 1 day
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_EXPIRATION))
                 .signWith(getSignInKey() , SignatureAlgorithm.HS256)
                 .compact();
     }
