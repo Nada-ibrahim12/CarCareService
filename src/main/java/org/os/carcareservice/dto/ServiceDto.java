8680ed538dae1935d6a5f6f1658e569d62a88923
malak <malakmadbouly69@gmail.com>
2025-08-27 20:00:58 +0300

    feat: update services controllers and DTOs

---
+++ src/main/java/org/os/carcareservice/dto/ServiceDto.java
@@ -0,0 +1,12 @@
+package org.os.carcareservice.dto;
+
+import java.math.BigDecimal;
+
+public class ServiceDTO {
+    private Integer id;
+    private String name;
+    private String status;
+    private BigDecimal price;
+    private Integer providerId; 
+
+}
